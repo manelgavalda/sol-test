cdec63ae5d0d88f3c414a0769c9310b9ce4642a2
manel <manelgavalda1@gmail.com>
2023-11-11 19:11:51 +0100

    showing price and balance

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import axios from "axios";
+import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));
-
 const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
 
 const balance = await connection.getBalance(address);
 
 console.log(`The balance of the account at ${address} is ${balance} lamports`);
+
+const solBalance = balance / LAMPORTS_PER_SOL
+
+const solPrice = await axios.get(
+  'https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd,eur',
+  { headers: {'accept-encoding': '*'} }
+).then(({data}) => data.solana.usd)
+
+console.log(`Balance is ${solBalance} | Price is $${solPrice} | Total is $${(solPrice * solBalance).toFixed(2)}`)
 
 console.log(`✅ Finished!`)
